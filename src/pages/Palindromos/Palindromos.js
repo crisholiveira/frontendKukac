c456eae537c8777f4a1baf5b36a718f77cacdb49
crisholiveira <cris.ho.soares@gmail.com>
2022-05-22 13:55:08 -0300

    state do formulario do palindromo

--- src/pages/Palindromos/Palindromos.js
+++ src/pages/Palindromos/Palindromos.js
@@ -3,18 +3,28 @@ import { Link } from "react-router-dom"
 import { useState, useEffect } from "react"
 
 const Palindromos = () => {
+const [numInicial, setNumInicial] = useState("")
+const [numFinal, setNumFinal] = useState("")
+
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    const intervalo = {
+      numInicial,
+      numFinal
+    }
+    console.log(intervalo)
   }
 
 
+
+  
   return (
-    <div>
+    <div id="palindromos">
       <h2>DesafioKukac</h2>
       <p className="subtitle">Desafio dos números palíndromos</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Digite o número inicial do intervalo" />
-        <input type="text" placeholder="Digite o número final do intervalo"/>
+        <input type="text" placeholder="Digite o número inicial do intervalo" onChange={(e) => setNumInicial(e.target.value)} value ={numInicial || ""}/>
+        <input type="text" placeholder="Digite o número final do intervalo" onChange={(e) => setNumFinal(e.target.value)} value ={numFinal || ""}/>
         <input type="submit" value="Exibir"/>
       </form>
       <p>Números palíndromos são aqueles que escritos da direita para esquerda ou da esquerda para direita tem o mesmo valor. Exemplo: 929, 44, 97379</p>
